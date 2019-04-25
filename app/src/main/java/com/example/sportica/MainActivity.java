b29d652d43fd022b6e7af101a90cf2ebe7e66097
Tharn Phongsavaree <gast.jung@hotmail.com>
2019-04-25 17:55:56 +0700

    NewsListActivity + readDatabase

--- app/src/main/java/com/example/sportica/MainActivity.java
+++ app/src/main/java/com/example/sportica/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.sportica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView sportica;
+    EditText sidEditText;
+    EditText passwordEditText;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sportica = this.findViewById(R.id.sportica);
+        sidEditText = this.findViewById(R.id.sidEditText);
+        passwordEditText = this.findViewById(R.id.passwordEditText);
+        loginButton = this.findViewById(R.id.loginButton);
 
-        // Hi gusts.
-        //Hello
+    }
+
+    public void loginClick(View v){
+        Intent intent = new Intent(this, NewsListActivity.class);
+        startActivity(intent);
     }
 }
