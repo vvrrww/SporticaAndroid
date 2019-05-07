c07d991d5d15f0c229bb9c5f00bf4510e309be01
vvrrww <ole1996@gmail.com>
2019-05-07 22:26:27 +0700

    SSO complete

--- app/src/main/java/com/example/sportica/LoginActivity.java
+++ app/src/main/java/com/example/sportica/LoginActivity.java
@@ -1,12 +1,19 @@
 package com.example.sportica;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "SSODemo";
@@ -38,5 +45,40 @@ public class LoginActivity extends AppCompatActivity {
         startActivityForResult(intent,ChulaSSOActivity.LOGIN);
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==ChulaSSOActivity.LOGIN && resultCode== Activity.RESULT_OK) {
+            // Ticket can be reuse until expired.
+            String ticket=data.getStringExtra("ticket");
+            Log.i(TAG,"ticket:"+ticket);
+            HelperTask helperTask=new HelperTask();
+            helperTask.execute(ticket);
+
+
+        }
+    }
+
+    class HelperTask extends AsyncTask<String, Void,String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String ticket=strings[0];
+            String json = ChulaSSOHelper.serviceValidation(ticket);
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+            super.onPostExecute(json);
+            try{
+                JSONObject user = new JSONObject(json);
+                myTextView.setText("Hello, "+user.getString("firstname")+" "+user.getString("lastname"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
 }
