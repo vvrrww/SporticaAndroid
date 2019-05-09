e83c97ebccf1940e33debd571a88df77f51aa71b
Tharn Phongsavaree <gast.jung@hotmail.com>
2019-05-09 15:11:08 +0700

    TournamentDetail
    + 4 Buttons

---
+++ app/src/main/java/com/example/sportica/TournamentMedalsActivity.java
@@ -0,0 +1,28 @@
+package com.example.sportica;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class TournamentMedalsActivity extends AppCompatActivity {
+
+    String key;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tournament_medals);
+
+        Intent intent = getIntent();
+        key = intent.getStringExtra("key");
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()== android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/sportica/TournamentSportActivity.java
@@ -0,0 +1,28 @@
+package com.example.sportica;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class TournamentSportActivity extends AppCompatActivity {
+
+    String key;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tournament_sport);
+
+        Intent intent = getIntent();
+        key = intent.getStringExtra("key");
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()== android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
